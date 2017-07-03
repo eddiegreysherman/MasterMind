82546d294a1bcc39f59ff7cded89a398da5fd19e
eddie.sherman@gmail.com <Gr3750n123>
2017-07-03 19:17:54 -0400

    trying to append a list of lists to attempts...but it is funky. gonna take a break.

--- mastermind.py
+++ mastermind.py
@@ -12,36 +12,39 @@ print(code)
 
 guess_code = []
 hints = []
+attempts = []
 
-i = 1
-while i <= 4:
-    num = input("Guess number {} in the pattern (Must be 1-6): ".format(i))
-    if not num.isnumeric():
-        print("Please enter a NUMBER, 1 - 6.")
-        continue
-    else:
-        num = int(num)
+while len(attempts) < 10:
+    i = 1
+    while i <= 4:
+        num = input("Guess number {} in the pattern (Must be 1-6): ".format(i))
+        if not num.isnumeric():
+            print("Please enter a NUMBER, 1 - 6.")
+            continue
+        else:
+            num = int(num)
 
-    if num in [1, 2, 3, 4, 5, 6]:
-        guess_code.append(num)
-        i += 1
-    else:
-        print("The number must be 1 through 6.")
+        if num in [1, 2, 3, 4, 5, 6]:
+            guess_code.append(num)
+            i += 1
+        else:
+            print("The number must be 1 through 6.")
 
-print(code)
+    print(code)
 
-def process_guess(a,b):
-    for i in range(4):
-        if a[i] == b[i]:
-            hints.append("B")
-        elif a[i] in b:
-            hints.append("W")
-    return hints
+    def process_guess(a,b):
+        for i in range(4):
+            if a[i] == b[i]:
+                hints.append("B")
+            elif a[i] in b:
+                hints.append("W")
+        return hints
 
-# trying to get a nested list going for display purposes...
-hints = process_guess(code, guess_code)
+    # trying to get a nested list going for display purposes...
+    hints = process_guess(code, guess_code)
 
-attempts = [guess_code, hints]
+    #attempts.append([[guess_code, hints]])
+    attempts.append(list([guess_code, hints]))
 
-print(attempts)
-
+    for _ in range(len(attempts)):
+        print(attempts)
